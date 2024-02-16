2ec21b16147454634b1021455d0f53ac96e84936
ac1002 <13202081@life.hkbu.edu.hk>
2024-02-17 01:01:18 +0800

    Initialize repository with boilerplate code for Chrome extension - Set up the basic directory structure for a Chrome extension. - Included manifest.json with necessary default settings.

---
+++ index.js
@@ -0,0 +1,15 @@
+const currencyList = {
+  HKD: { symbol: "HKD", name: "Hong Kong Dollar" },
+  AUD: { symbol: "AUD", name: "Australian Dollar" },
+  CAD: { symbol: "CAD", name: "Canadian Dollar" },
+  CHF: { symbol: "CHF", name: "Swiss Franc" },
+  CNY: { symbol: "CNY", name: "Chinese Yuan" },
+  DKK: { symbol: "DKK", name: "Danish Krone" },
+  EUR: { symbol: "EUR", name: "Euro" },
+  GBP: { symbol: "GBP", name: "British Pound" },
+  JPY: { symbol: "JPY", name: "Japanese Yen" },
+  KRW: { symbol: "KRW", name: "South Korean Won" },
+  PLN: { symbol: "PLN", name: "Polish Zloty" },
+  TRY: { symbol: "TRY", name: "Turkish New Lira" },
+  USD: { symbol: "USD", name: "United States Dollar" },
+};
